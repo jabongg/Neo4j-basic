944435fd9ebeff042d62501aa4d2578b9e8ccc6e
Jangbahadur Patel <>
2019-12-24 00:06:59 +0530

    pair with given sum

---
+++ src/main/java/geeksforgeeks/arrays/ContiguousArray.java
@@ -0,0 +1,8 @@
+package geeksforgeeks.arrays;
+
+public class ContiguousArray {
+    //Given an unsorted array A of size N of non-negative integers,
+    // find a continuous sub-array which adds to a given number S.
+
+
+}
---
+++ src/main/java/geeksforgeeks/arrays/PairAsSum.java
@@ -0,0 +1,41 @@
+package geeksforgeeks.arrays;
+
+import java.util.Arrays;
+
+/**
+ * Given an array A[] and a number x, check for pair in A[] with sum as x
+ */
+public class PairAsSum {
+    public static void main(String[] args) {
+
+        int[] A = {1, 4, 45, 6, 10, -8};
+        int K = 2;
+
+        System.out.println(hasArrayTwoCandidates(A, A.length, K));
+
+    }
+
+    // Function to check if array has 2 elements
+    // whose sum is equal to the given value
+    static boolean hasArrayTwoCandidates(int A[], int arr_size, int sum) {
+        Arrays.sort(A); // sort the array first
+        return containsPairWithGivenSum(A, sum);
+    }
+
+    static boolean containsPairWithGivenSum(int[] A, int sum) {
+        int i = 0;
+        int j = A.length - 1;
+
+        while (i < j) {
+            if (A[i] + A[j] == sum) {
+                return true;
+            }
+            if (A[i] + A[j] > sum) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
