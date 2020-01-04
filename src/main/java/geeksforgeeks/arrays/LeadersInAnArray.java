a76b049d858faa6d4370ea5bff007556d8678244
Jangbahadur Patel <>
2020-01-04 20:48:12 +0530

    trapping rain water

--- src/main/java/geeksforgeeks/arrays/LeadersInAnArray.java
+++ src/main/java/geeksforgeeks/arrays/LeadersInAnArray.java
@@ -20,15 +20,35 @@ class LeadersInAnArray {
                 A[j] = scan.nextInt();
             }
 
-            findLeaders(A);
+            leadersInArray(A);
         }
     }
 
+    // O(n^2) time complexity
+    public static void findLeadersInArray(int[] A) {
+        for (int i = 0; i < A.length - 1; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] < A[j]) {
+                    break;
+                }
+
+                if (j == A.length - 1) {
+                    System.out.print(A[i] + " ");
+                }
+            }
+        }
+
+        System.out.print(A[A.length - 1]);
+        System.out.println();
+
+    }
+
+    // better O(n) time complexity but TLE
     public static void findLeaders(int[] A) {
         ArrayList<Integer> leaders = new ArrayList<>();
         int currentLeader = A[A.length - 1];
         for (int j = A.length - 1; j >= 0; j--) {
-            if (A[j] > currentLeader) {
+            if (A[j] >= currentLeader) {
                 //System.out.print(currentLeader + " ");
                 leaders.add(currentLeader);
                 currentLeader = A[j];
@@ -44,6 +64,31 @@ class LeadersInAnArray {
 
         for (Integer el : leaders) {
             System.out.print(el + " ");
+        }
+
+        System.out.println();
+    }
+
+
+    // better O(n) time complexity
+    public static void leadersInArray(int[] A) {
+        int potentialLeader = A[0];
+        int currentMaxElement;
+        for (int j = 0; j <= A.length - 1; j++) {
+            if (A[j] >= potentialLeader) {
+                potentialLeader = A[j];
+
+                currentMaxElement  = (A[j + 1]);
+                if (currentMaxElement < A[j]) {
+                    System.out.print(potentialLeader + " ");
+                    potentialLeader = A[j];
+                }
+            }
+            //potentialLeader = A[j + 1];
+            if (j == A.length - 1) {
+                //System.out.print(potentialLeader +" ");
+                System.out.print(A[A.length - 1]);
+            }
         }
 
         System.out.println();
---
+++ src/main/java/geeksforgeeks/arrays/TrappingRainWater.java
@@ -0,0 +1,51 @@
+package geeksforgeeks.arrays;
+
+import java.util.Scanner;
+
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        //code
+
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = scan.nextInt();
+            int[] A = new int[N];
+
+            for (int j = 0; j < N; j++) {
+                A[j] = scan.nextInt();
+            }
+
+            int trappedWaterVolume = findTrappedWaterVolume(A);
+            System.out.println(trappedWaterVolume);
+        }
+    }
+
+    //7 4 0 19 0 5 10
+    private static int findTrappedWaterVolume(int[] A) {
+        int left = 0, leftMax = 0, rightMax = 0, trappedRainWaterVolume = 0;
+        int right = A.length - 1;
+
+        while (left < right) {
+            if (A[left] < A[right]) {
+                if (A[left] > leftMax) {
+                    leftMax = A[left];
+                } else {
+                    trappedRainWaterVolume = trappedRainWaterVolume + (leftMax - A[left]);
+                }
+                left++;
+            }
+
+            else {
+                if (A[right] > rightMax) {
+                    rightMax = A[right];
+                } else {
+                    trappedRainWaterVolume = trappedRainWaterVolume + (rightMax - A[right]);
+                }
+                right--;
+            }
+        }
+        return trappedRainWaterVolume;
+    }
+}
